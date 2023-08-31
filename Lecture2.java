83a1a72330ee16c161eee14ef7a2c59566bbb8bb
mig29pandas <manda.kausthubh@iiitb.ac.in>
2023-08-31 20:57:13 +0530

    Updating the Lecture2 file...

--- Lecture2.java
+++ Lecture2.java
@@ -1,13 +1,16 @@
 class Lecture2
 {
-    public static main(String Args[])
+    public static void main(String Args[])
     {
         // Comments
         int a = 10;
         int arr[];
         arr = new int[10];
+        for (int i = 0; i < 9; i++){
+            arr[i] = i;
+        }
         float b = (float)123;  //Type casting is compulsary ...
-        System.out.println(arr);
+        System.out.println(arr[0], arr[1], arr[2]);
 
         // Strings
         String str = new String("Hello World!!");
